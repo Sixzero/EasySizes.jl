8bc3bea5896e3f95c419d47a82879dc669f72510
SixZero <havliktomi@hotmail.com>
2023-11-05 19:44:21 +0100

    Initial weight comparison codes.

---
+++ README.md
@@ -0,0 +1,18 @@
+# EasySizes.jl
+
+A simple lightweight package. 
+EasySizes provides simple utilities for comparing the length or size dimensions of arrays against specified values.
+
+## Installation
+
+```
+add EasySizes
+```
+
+## Usages
+```julia
+arr = [1, 2, 3]
+arr > 2Len  # equals length(arr) > 2
+```
+
+
--- src/EasySizes.jl
+++ src/EasySizes.jl
@@ -1,5 +1,19 @@
 module EasySizes
 
+export Len, S
 greet() = print("Hello World!")
+struct Len v::Int end
+Base.:(*)(v::Int, ::Type{Len}) = Len(v)
+Base.isless(x, c::Len) = isless(length(x), c.v)
+Base.isless(c::Len, x) = isless(c.v, length(x))
+Base.:(==)(x, c::Len) = length(x) == c.v
+Base.:(==)(c::Len, x) = c.v == length(x)
+
+struct S{N} v::Int end
+Base.:(*)(v::Int, ::Type{S{N}}) where{N} = S{N}(v)
+Base.isless(x, c::S{N}) where {N} = isless(size(x, N), c.v)
+Base.isless(c::S{N}, x) where {N} = isless(c.v, size(x, N))
+Base.:(==)(x, c::S{N}) where {N} = size(x, N) == c.v
+Base.:(==)(c::S{N}, x) where {N} = c.v == size(x, N)
 
 end # module EasySizes
